888fdd692bfea1a84e55eb8d757eb31b7cbc631a
Brahim El Ghachi <belghachi@gmail.com>
2025-10-27 17:08:43 +0100

    OchatDay1

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,49 @@
 <script>
+  import "./app.css";
 </script>
 
-<p>Bienvenue en saison A07 ! (Enlève ce paragraphe pour écrire ton code HTML à la place 😉)</p>
+<main>
+  
+  <div class="layout">
+  <!-- Header -->
 
-<style>
-</style>
+  <header class="header">
+
+    <div class="logo-area">
+      <img src="" alt="Logo chat" class="logo" />
+      <h1 class="app-title">O'CHAT</h1>
+    </div>
+    
+    <div class="title-area">
+    <h2 class="chat-title">Chat Title</h2>
+    </div>
+  </header>
+
+  <main class="main">
+    <!-- Sidebar -->
+    <aside class="sidebar">
+      <button class="chat-item">Chat 1</button>
+      <button class="chat-item">Chat 2</button>
+      <button class="chat-item">Chat 3</button>
+      <button class="chat-item">Chat 4</button>
+    </aside>
+
+    <!-- Chat Content -->
+    <section class="chat">
+      <h3>Messages</h3>
+
+      <div class="messages">
+        <div class="message message-left"></div>
+        <div class="message message-right"></div>
+        <div class="message message-left"></div>
+      </div>
+
+      <!-- Message Input -->
+      <footer class="input-area">
+        <input type="text" placeholder="Message input" class="message-input" />
+        <button class="send-button">Créer</button>
+      </footer>
+    </section>
+  </main>
+</div>
+</main>
