78519d98ca1106980e0e4e4610bf583c830ddc29
Brahim El Ghachi <belghachi@gmail.com>
2025-10-29 10:40:03 +0100

    O'ChatDay3

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,91 @@
 <script>
+  import { onMount } from "svelte";
   import "./app.css";
+  
+  localStorage.setItem("token", "j9hEz9GGkoYnElQmkGnkXwGTncS3TU2l");
+  const token = localStorage.getItem("token");
+
+  // Création de la variable pour envoi du formulaire vers l'API du Chat
+  let messagesInput = $state("");
+
+  // Création d'une variable pour stocker les réponses de l'IA Mistral
+  let messageAPI = $state([""]);
+  
+
+  // Fonction pour envoyer requête (formulaire vers API Mistral)
+  async function handleSubmitForm(event) {
+    event.preventDefault(); // Fonction pour envoyer requête (formulaire vers API Mistral)t();
+
+    fetch("https://api.mistral.ai/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+
+      body: JSON.stringify({
+        model: "mistral-medium-latest",
+        messages: [{ role: "user", content: (messagesInput)}],
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        messageAPI = data.choices[0].message.content;
+        console.log(`Réponse IA: ${messageAPI}`);
+      });
+  }
+  
+
 </script>
 
-<main>
-  
-  <div class="layout">
+<div class="layout">
   <!-- Header -->
-
   <header class="header">
-
     <div class="logo-area">
-      <img src="" alt="Logo chat" class="logo" />
+      <img src="/chat.jpeg" alt="Logo chat" class="logo" />
       <h1 class="app-title">O'CHAT</h1>
     </div>
-    
     <div class="title-area">
-    <h2 class="chat-title">Chat Title</h2>
+      <h2 class="chat-title">Bienvenue sur O'Chat</h2>
     </div>
   </header>
 
-  <main class="main">
+  <section class="main">
     <!-- Sidebar -->
     <aside class="sidebar">
-      <button class="chat-item">Chat 1</button>
-      <button class="chat-item">Chat 2</button>
-      <button class="chat-item">Chat 3</button>
-      <button class="chat-item">Chat 4</button>
+      <button class="chat-button">Chat 1</button>
+      <button class="chat-button">Chat 2</button>
+      <button class="chat-button">Chat 3</button>
+      <button class="chat-button">Chat 4</button>
     </aside>
 
     <!-- Chat Content -->
-    <section class="chat">
+    <div class="chat">
       <h3>Messages</h3>
 
-      <div class="messages">
-        <div class="message message-left"></div>
-        <div class="message message-right"></div>
-        <div class="message message-left"></div>
+      <div class="messages-field">
+        <p class="message-user">{messagesInput}</p>
+        <p class="message-bot">
+          {#each messageAPI as message}
+          {message}
+          {/each}
+        </p>
+        <p></p>
       </div>
 
       <!-- Message Input -->
+
       <footer class="input-area">
-        <input type="text" placeholder="Message input" class="message-input" />
-        <button class="send-button">Créer</button>
+        <form onsubmit={handleSubmitForm}>
+          <input
+            type="text"
+            id="messageInput"
+            placeholder="Pose ta question içi"
+            class="message-input"
+            bind:value={messagesInput} />
+          <button type="submit" class="send-button">Créer</button>
+        </form>
       </footer>
-    </section>
-  </main>
+    </div>
+  </section>
 </div>
-</main>
